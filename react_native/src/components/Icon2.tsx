92dd73c84727c08b63a6f4b5e997b0a3decffdfa
elliottgorrell <elliottgorrell1@gmail.com>
2024-10-26 15:24:33 +0200

    Adds atom labs element basic components which will replace ui kitten

---
+++ react_native/src/components/Icon2.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+
+export interface IconT {
+  name: keyof typeof Ionicons.glyphMap;
+  size: number;
+  color: string;
+  style?: object;
+}
+const Icon = ({ color, name, size, style }: IconT): React.JSX.Element => (
+  <Ionicons name={name} size={size} color={color} style={style} />
+);
+
+export default Icon;
